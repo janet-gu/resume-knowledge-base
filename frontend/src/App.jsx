3a44d9796802e7a4404bee45fe5ed12f17bd45e7
janet-gu <janetgu@tencent.com>
2026-02-10 10:48:38 +0800

    Create App component with navigation and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Contact = () => <div>Contact Page</div>;
+
+const App = () => {
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/" component={Home} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
